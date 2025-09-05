01c43f2507891d43a073ad4227fa21c2fe8a61f3
CodeX047 <vishalrp047@gmail.com>
2025-09-05 19:55:50 +0530

    Add hero section scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
 import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef()
+
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+
   useGSAP(() => {
     const heroSplit = new SplitText('.title', { type: 'chars, words' })
     const paragraphSplit = new SplitText('.subtitle', { type: 'lines' })
@@ -35,11 +41,32 @@ const Hero = () => {
     })
       .to('.left-leaf', { y: -200 }, 0)
       .to('.right-leaf', { y: 200 }, 0)
+    
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';
+
+    const video = videoRef.current;
+
+    const videoTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      videoTimeline.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, [])
 
   return (
     <>
-      <section id="hero" className="noisy">
+      <section id="hero" className=""> //noisy class can be add if needed 
         <h1 className="title">MOJITO</h1>
 
         <img
@@ -74,6 +101,11 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video ref={videoRef} src="/videos/output.mp4" muted playsInline preload="auto" />
+      </div>
+      
     </>
   );
 }
