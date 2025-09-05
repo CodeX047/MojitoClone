faea4069778b1eed0d3f99bc6d1763fd65fdf8e7
CodeX047 <vishalrp047@gmail.com>
2025-09-05 20:01:45 +0530

    Add noisy class to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -66,21 +66,18 @@ const Hero = () => {
 
   return (
     <>
-      <section id="hero" className=""> //noisy class can be add if needed 
+      <section id="hero" className="noisy">
         <h1 className="title">MOJITO</h1>
-
         <img
           src="/images/hero-left-leaf.png"
           alt="left-leaf"
           className="left-leaf"
         />
-
         <img
           src="/images/hero-right-leaf.png"
           alt="right-leaf"
           className="right-leaf"
         />
-
         <div className="body">
           <div className="content">
             <div className="space-y-5 hidden md:block">
@@ -103,9 +100,14 @@ const Hero = () => {
       </section>
 
       <div className="video absolute inset-0">
-        <video ref={videoRef} src="/videos/output.mp4" muted playsInline preload="auto" />
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
       </div>
-      
     </>
   );
 }
